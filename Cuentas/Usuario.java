4558759da4b1a31bf599ff2757c80e69d86943e4
coderfirstacc <143194670+coderfirstacc@users.noreply.github.com>
2024-11-16 18:10:58 -0600

    Update Usuario.java

--- Cuentas/Usuario.java
+++ Cuentas/Usuario.java
@@ -1,4 +1,26 @@
 public class Usuario{
-  String Nombre
-  String Password
+  String Nombre;
+  String Password;
+  
+  public Usuario(String Nombre,String Password){
+    this.Nombre = Nombre;
+    this.Password = Password;
+  }
+
+  public String getNombre(){
+    return this.Nombre;
+  }
+
+  public void setNombre(String Nombre){
+    this.Nombre = Nombre;
+  }
+
+  public String getPassword(){
+    return this.Nombre;
+  }
+
+  public void setPassword(String Password){
+    this.Nombre = Nombre;
+  }
+
 }
