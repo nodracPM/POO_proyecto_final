79653301104e9a6209548ca9f97d96f668e7d593
coderfirstacc <143194670+coderfirstacc@users.noreply.github.com>
2024-11-17 20:52:18 -0600

    Update Ticket.java

--- programa/usuarios/Ticket.java
+++ programa/usuarios/Ticket.java
@@ -1,18 +1,23 @@
-package programa.usuarios;
+//package programa.usuarios;
 
 import java.io.*;
+import java.util.Date;
 
-public class Ticket implements Serializable{
+public class Ticket implements Serializable,Cloneable{
     private int Asientos;
     private String Propietario;
-    private String FechaCompra;
-    //private Evento evento;
+    private Date FechaCompra;
+    private Date FechaEvento;
+    private String hora;
+    private String local;
 
-    public Ticket (int Asientos,String Propietario,String FechaCompra/* ,Evento evento*/){
+    public Ticket (int Asientos,String Propietario,Date FechaCompra,Date FechaEvento,String hora,String local){
         this.Asientos=Asientos;
         this.Propietario=Propietario;
         this.FechaCompra=FechaCompra;
-        //this.evento = evento;
+        this.FechaEvento=FechaEvento;
+        this.hora=hora;
+        this.local=local;
     }
 
     @Override
@@ -20,6 +25,14 @@ public class Ticket implements Serializable{
         System.out.println("Asientos: " +this.Asientos);
         System.out.println("Propietario: " +this.Propietario);
         System.out.println("Fecha de compra: " +this.FechaCompra);
+        System.out.println("Fecha de evento: "+this.FechaEvento);
+        System.out.println("Hora del evento: "+this.hora);
+        System.out.println("Lugar del evento: "+this.local);
         return "Gracias por su reservación";
+    }
+
+    @Override
+    public Ticket clone(){
+        return new Ticket(this.Asientos, this.Propietario, this.FechaCompra,this.FechaEvento,this.hora,this.local);
     }
 }
